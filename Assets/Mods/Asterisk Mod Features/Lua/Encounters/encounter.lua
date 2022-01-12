4a1e433f90a0dfa69672a2545f460e1a2ef920e8
Fennene <fnfennene256@gmail.com>
2022-01-12 13:00:50 +0900

    10 Add Example of setting buttons' posiiton

--- Assets/Mods/Asterisk Mod Features/Lua/Encounters/encounter.lua
+++ Assets/Mods/Asterisk Mod Features/Lua/Encounters/encounter.lua
@@ -14,14 +14,26 @@ enemypositions = {
 possible_attacks = {"bullettest_bouncy", "bullettest_chaserorb", "bullettest_touhou"}
 
 function EncounterStarting()
+    -- If player launch this mod with official CYF, show error
     if isModifiedCYF == nil then
         error("This game can not be launched on official CYF.\nUse to lanuch CYF-AsteriskMod.")
     end
     if Asterisk == nil then
         error("This game can be launched on only CYF-AsteriskMod.")
     end
+
     Player.name = "FN"
+
+    -- Set activate of buttons
     ButtonUtil.SetActives(true, true, true, false)
+    -- Move all buttoms to center
+    ButtonUtil.FIGHT.MoveTo(234, 0)
+    ButtonUtil.ACT.MoveTo(81, 0)
+    ButtonUtil.ITEM.MoveTo(-79, 0)
+    --ButtonUtil.MERCY.MoveTo(-234, 0)
+    -- Move
+    ButtonUtil.FIGHT.Move(-187, 0)
+    ButtonUtil.ITEM.Move(187, 0)
 end
 
 function EnemyDialogueStarting()
