5aa97bacd1f672d8789054f8b11635d69d2beeb4
Fennene <fnfennene256@gmail.com>
2022-04-08 14:51:20 +0900

    71 AddExample00

---
+++ Assets/Mods/Asterisk Mod Features/Lua/Encounters/00 - Copying Error Message.lua
@@ -0,0 +1,19 @@
+-- You need to check that the player use AsteriskMod.
+if Asterisk == nil then
+    error("This mod can be launched on only CYF-AsteriskMod."
+       .. "\nAsteriskMod -> https://github.com/Fennene/CreateYourFrisk-AsteriskMod"
+    )
+end
+error(
+    "\n" ..
+    "\n" ..
+    "\n" ..
+    "\n" ..
+    "-----------------------------------------------------------------------------------------------\n" ..
+    "\n" ..
+    "You can copy this error message by pressing \"C\" key!\n" ..
+    "Try to copy and paste to notepad.\n" ..
+    "\n" ..
+    "-----------------------------------------------------------------------------------------------\n" ..
+    "\n"
+)
