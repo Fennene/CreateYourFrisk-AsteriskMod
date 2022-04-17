aae86ff3256a825abe7fe983488d266baa398174
Fennene <fnfennene256@gmail.com>
2022-04-17 11:38:21 +0900

    101-1 StaticTextManager (Add)

---
+++ Assets/Scripts/AsteriskMod/AsteriskUtil.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using MoonSharp.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Object = UnityEngine.Object;
+
+namespace AsteriskMod
+{
+    public static class AsteriskUtil
+    {
+        public static float CalcTextWidth(StaticTextManager txtmgr, int fromLetter = -1, int toLetter = -1, bool countEOLSpace = false, bool getLastSpace = false)
+        {
+            float totalWidth = 0, totalWidthSpaceTest = 0, totalMaxWidth = 0, hSpacing = txtmgr.Charset.CharSpacing;
+            if (fromLetter == -1) fromLetter = 0;
+            if (txtmgr.textQueue == null) return 0;
+            if (txtmgr.textQueue[txtmgr.currentLine] == null) return 0;
+            if (toLetter == -1) toLetter = txtmgr.textQueue[txtmgr.currentLine].Text.Length - 1;
+            if (fromLetter > toLetter || fromLetter < 0 || toLetter > txtmgr.textQueue[txtmgr.currentLine].Text.Length) return -1;
+
+            for (int i = fromLetter; i <= toLetter; i++)
+            {
+                switch (txtmgr.textQueue[txtmgr.currentLine].Text[i])
+                {
+                    case '[':
+
+                        if (txtmgr.Charset.Letters.ContainsKey(txtmgr.textQueue[txtmgr.currentLine].Text[i]))
+                            totalWidth += txtmgr.Charset.Letters[txtmgr.textQueue[txtmgr.currentLine].Text[i]].textureRect.size.x + hSpacing;
+                        break;
+                    case '\r':
+                    case '\n':
+                        if (totalMaxWidth < totalWidthSpaceTest - hSpacing)
+                            totalMaxWidth = totalWidthSpaceTest - hSpacing;
+                        totalWidth = 0;
+                        totalWidthSpaceTest = 0;
+                        break;
+                    default:
+                        if (txtmgr.Charset.Letters.ContainsKey(txtmgr.textQueue[txtmgr.currentLine].Text[i]))
+                        {
+                            totalWidth += txtmgr.Charset.Letters[txtmgr.textQueue[txtmgr.currentLine].Text[i]].textureRect.size.x + hSpacing;
+                            // Do not count end of line spaces
+                            if (txtmgr.textQueue[txtmgr.currentLine].Text[i] != ' ' || countEOLSpace)
+                                totalWidthSpaceTest = totalWidth;
+                        }
+                        break;
+                }
+            }
+            if (totalMaxWidth < totalWidthSpaceTest - hSpacing)
+                totalMaxWidth = totalWidthSpaceTest - hSpacing;
+            return totalMaxWidth + (getLastSpace ? hSpacing : 0);
+        }
+
+        public static float CalcTextHeight(StaticTextManager txtmgr, int fromLetter = -1, int toLetter = -1)
+        {
+            float maxY = -999, minY = 999;
+            if (fromLetter == -1) fromLetter = 0;
+            if (toLetter == -1) toLetter = txtmgr.textQueue[txtmgr.currentLine].Text.Length;
+            if (fromLetter > toLetter || fromLetter < 0 || toLetter > txtmgr.textQueue[txtmgr.currentLine].Text.Length) return -1;
+            if (fromLetter == toLetter) return 0;
+            for (int i = fromLetter; i < toLetter; i++)
+            {
+                if (!txtmgr.Charset.Letters.ContainsKey(txtmgr.textQueue[txtmgr.currentLine].Text[i])) continue;
+                if (txtmgr.letterPositions[i].y < minY)
+                    minY = txtmgr.letterPositions[i].y;
+                if (txtmgr.letterPositions[i].y + txtmgr.Charset.Letters[txtmgr.textQueue[txtmgr.currentLine].Text[i]].textureRect.size.y > maxY)
+                    maxY = txtmgr.letterPositions[i].y + txtmgr.Charset.Letters[txtmgr.textQueue[txtmgr.currentLine].Text[i]].textureRect.size.y;
+            }
+            return maxY - minY;
+        }
+    }
+}
---
+++ Assets/Scripts/AsteriskMod/Text/InstantTextMessage.cs
@@ -0,0 +1,38 @@
+﻿using MoonSharp.Interpreter;
+
+namespace AsteriskMod
+{
+    public class InstantTextMessageT : TextMessage
+    {
+        public InstantTextMessageT(string text) : base(text, false, true) { }
+    }
+
+    public class InstantTextMessage
+    {
+        public InstantTextMessage(string text, bool actualText = true)
+        {
+            Setup(text, false, true, actualText);
+        }
+
+        public string Text { get; set; }
+        public bool Decorated { get { return false; } }
+        public bool ShowImmediate { get { return true; } }
+        public bool ActualText { get; private set; }
+
+        protected void Setup(string text, bool decorated, bool showImmediate, bool actualText)
+        {
+            text = Unescape(text); // compensate for unity inspector autoescaping control characters
+            text = text.Replace("[name]", PlayerCharacter.instance.Name);
+            Text = text;
+            ActualText = actualText;
+        }
+
+        public void SetText(string text) { Text = text; }
+
+        private static string Unescape(string str)
+        {
+            try { return str.Replace("\\n", "\n").Replace("\\r", "\r").Replace("\\t", "\t"); }
+            catch { return str; }
+        }
+    }
+}
